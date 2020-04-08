ef7dbc5f6335f5375569f2c1449c1bb177979490
Daniel <daniel.gru17@gmail.com>
2020-04-08 17:59:36 +0300

    Commit pom classes

--- src/main/java/HelloWorldAutomation.java
+++ src/main/java/HelloWorldAutomation.java
@@ -1,5 +1,9 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class HelloWorldAutomation {
 
@@ -14,6 +18,16 @@ public class HelloWorldAutomation {
         driver.get(baseURL);
         driver.manage().window().maximize();
         System.out.println(driver.getTitle());
+
+        WebElement shiftContent = driver.findElement(By.linkText("Shifting Content"));
+        shiftContent.click();
+
+        WebElement example1 = driver.findElement(By.linkText("Example 1: Menu Element"));
+        example1.click();
+
+        List<WebElement> countMenu = driver.findElements(By.tagName("li"));
+        System.out.println(countMenu.size());
+
         driver.quit();
     }
 
