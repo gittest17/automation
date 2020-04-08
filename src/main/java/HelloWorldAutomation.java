0ef91a71ea14f3227adcd1922d956eca622e8cbb
Daniel <daniel.gru17@gmail.com>
2020-04-08 14:55:14 +0300

    Commit pom resources

--- src/main/java/HelloWorldAutomation.java
+++ src/main/java/HelloWorldAutomation.java
@@ -1,2 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HelloWorldAutomation {
+
+    private WebDriver driver;
+    public String baseURL = "https://the-internet.herokuapp.com/";
+
+
+    public void setUp(){
+
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get(baseURL);
+        System.out.println(driver.getTitle());
+    }
+
+    public static void main(String[] args) {
+        HelloWorldAutomation test = new HelloWorldAutomation();
+        test.setUp();
+    }
 }
