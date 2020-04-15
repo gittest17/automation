71af043a70ecca67be1c46ee3bae361f14eeafdd
Daniel <daniel.gru17@gmail.com>
2020-04-15 22:56:30 +0300

    Dropdown Test

--- src/test/java/dropdown/DropDownTest.java
+++ src/test/java/dropdown/DropDownTest.java
@@ -2,16 +2,22 @@ package dropdown;
 
 import base.BasePage;
 import org.testng.annotations.Test;
-import pages.DropDownPage;
+import  pages.DropDownPage;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import java.util.List;
 
 public class DropDownTest extends BasePage {
 
     @Test
     public void testSelectOption(){
         DropDownPage dropDownPage = homePage.clickDropDown();
-        dropDownPage.selectFromDropDown("Option 1");
-        selectedOptions = dropDownPage.getSelectedOptions();
-        assertEquals(selectedOptions.
+        String option = "Option 1";
+        dropDownPage.selectFromDropDown(option);
+        List<String> selectedOptions = dropDownPage.getSelectedOptions();
+        assertEquals(selectedOptions.size(), 1, "Incorrect number of selections");
+        assertTrue(selectedOptions.contains(option), "Option not selected");
 
     }
 
